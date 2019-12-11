3d2dd01798338682ab807e444ae2b469d9c15f3b
jimliuxinghai <jimliuxinghai@163.com>
2019-12-11 23:57:28 +0800

    winston

--- server/log/logger.js
+++ server/log/logger.js
@@ -1,72 +1,194 @@
-const log4js = require('log4js');
-const path = require('path');
-const access = require("./access.js");
-const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
-const logDir = path.join(__dirname, '../../logs')
+// const log4js = require('log4js');
+// const path = require('path');
+// const access = require("./access.js");
+// const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
+// const logDir = path.join(__dirname, '../../logs')
 
-const baseInfo = {
-    appLogLevel: 'debug',
-    dir: 'logs',
-    env: 'dev',
-    projectName: 'Bumblebee'
-}
+// const baseInfo = {
+//     appLogLevel: 'debug',
+//     dir: 'logs',
+//     env: 'dev',
+//     projectName: 'Bumblebee'
+// }
 
-module.exports = (options) => {
+// module.exports = (options) => {
 
-    const contextLogger = {}
-    const appenders = {}
+//     const contextLogger = {}
+//     const appenders = {}
 
-    const opts = Object.assign({}, baseInfo, options || {})
-    const { env, appLogLevel, dir, projectName } = opts
-    const commonInfo = { projectName }
-    appenders.cheese = {
-        type: 'dateFile',
-        filename: `${logDir}/access.log`,
-        pattern: 'yyyyMMdd',
-        alwaysIncludePattern: true
-    }
+//     const opts = Object.assign({}, baseInfo, options || {})
+//     const { env, appLogLevel, dir, projectName } = opts
+//     const commonInfo = { projectName }
+//     appenders.cheese = {
+//         type: 'dateFile',
+//         filename: `${logDir}/access.log`,
+//         pattern: 'yyyyMMdd',
+//         alwaysIncludePattern: true
+//     }
 
-    if (env.match("development")) {
-        appenders.out = {
-            type: "console"
-        }
-    }
+//     if (env.match("development")) {
+//         appenders.out = {
+//             type: "console"
+//         }
+//     }
     
-    let config = {
-        appenders,
-        categories: {
-            default: {
-                appenders: Object.keys(appenders),
-                level: appLogLevel
-            }
-        },
-        replaceConsole: true,
-        pm2: true, 
-        disableClustering: true
-    }
+//     let config = {
+//         appenders,
+//         categories: {
+//             default: {
+//                 appenders: Object.keys(appenders),
+//                 level: appLogLevel
+//             }
+//         },
+//         replaceConsole: true,
+//         pm2: true, 
+//         disableClustering: true
+//     }
 
-    log4js.configure(config);
-    log4js.addLayout('json', function (config) {
-        return function (logEvent) { return JSON.stringify(logEvent) + config.separator; }
-    });
+//     log4js.configure(config);
+//     log4js.addLayout('json', function (config) {
+//         return function (logEvent) { return JSON.stringify(logEvent) + config.separator; }
+//     });
 
     
-    const logger = log4js.getLogger('[HTTP:ACCESS]');
+//     const logger = log4js.getLogger('[HTTP:ACCESS]');
+
+//     return async (ctx, next) => {
+//         const start = Date.now()
+//         methods.forEach((method, i) => {
+//             contextLogger[method] = (message) => {
+//                 logger[method](access(ctx, message, commonInfo))
+//             }
+//         })
+
+//         ctx.log = contextLogger;
+//         await next()
+//         const responseTime = Date.now() - start;
+        
+//         logger.info(access(ctx, {
+//             responseTime: `响应时间为${responseTime / 1000}s`
+//         }, commonInfo))
+//     }
+// }
+
+const winston = require('winston');
+const onFinished = require('on-finished');
+const { format } = require('util');
+
+const {
+    generateSchema,
+    generateFormat,
+    defaultSchemas,
+} = require('./stringify_schema');
+
+const {
+    createLogger,
+    format: { combine: wfcombine, printf: wfprintf },
+} = winston;
+
+const C = {
+    INFO: 'info',
+    WARN: 'warn',
+    ERROR: 'error',
+    MSG: 'HTTP %s %s',
+};
+
+const getLogLevel = (statusCode = 200, defaultLevel = C.INFO) => {
+    switch (Math.floor(statusCode / 100)) {
+        case 5:
+            return C.ERROR;
+        case 4:
+            return C.WARN;
+        default:
+            return defaultLevel;
+    }
+};
+
+/**
+ * logger middleware for koa2 use winston
+ *
+ * @param {object} [payload={}] - input arguments
+ * @param {object[]} [payload.transports=[new winston.transports.Stream({ stream: process.stdout })]] customize transports
+ * @param {string} [payload.level='info'] - default log level of logger
+ * @param {string} [payload.reqKeys=['header','url','method','httpVersion', 'href', 'query', 'length']] - default request fields to be logged
+ * @param {string} [payload.reqSelect=[]] - additional request fields to be logged
+ * @param {string} [payload.reqUnselect=['header.cookie']] - request field will be removed from the log
+ * @param {string} [payload.resKeys=['header', 'status']] - default response fields to be logged
+ * @param {string} [payload.resSelect=[]] - additional response fields to be logged
+ * @param {string} [payload.resUnselect=[]] - response field will be removed from the log
+ * @param {winston.transports.StreamTransportInstance} [payload.logger] - customize winston logger
+ * @param {string} [payload.msg=HTTP %s %s] - customize log msg
+ * @return {function} logger middleware
+ * @example
+ * const { logger } = require('koa2-winston');
+ * app.use(logger());
+ * // request logger look like down here
+ * // {
+ * //   "req": {
+ * //     "header": {
+ * //       "host": "127.0.0.1:59534",
+ * //       "accept-encoding": "gzip, deflate",
+ * //       "user-agent": "node-superagent/3.5.2",
+ * //       "connection": "close"
+ * //     },
+ * //     "url": "/",
+ * //     "method": "GET",
+ * //     "href": "http://127.0.0.1:59534/",
+ * //     "query": {}
+ * //   },
+ * //   "started_at": 1494486039864,
+ * //   "res": {
+ * //     "header": {
+ * //       "content-type": "text/plain; charset=utf-8",
+ * //       "content-length": "8"
+ * //     },
+ * //     "status": 200
+ * //   },
+ * //   "duration": 26,
+ * //   "level": "info",
+ * //   "message": "HTTP GET /"
+ * // }
+ */
+module.exports = (payload = {}) => {
+    const {
+        transports = [new winston.transports.Stream({ stream: process.stdout })],
+        level: defaultLevel = C.INFO,
+        msg = C.MSG,
+    } = payload;
+
+    // @ts-ignore
+    const stringifyFormat = generateFormat(payload);
+    const winstonLogger = payload.logger
+        || createLogger({
+            transports,
+            format: wfcombine(wfprintf(stringifyFormat)),
+        });
+
+    const onResponseFinished = (ctx, info) => {
+        info.res = ctx.response;
+        info.duration = Date.now() - info.started_at;
+
+        info.level = getLogLevel(info.res.status, defaultLevel);
+        // @ts-ignore
+        winstonLogger.log(info);
+    };
 
     return async (ctx, next) => {
-        const start = Date.now()
-        methods.forEach((method, i) => {
-            contextLogger[method] = (message) => {
-                logger[method](access(ctx, message, commonInfo))
-            }
-        })
+        const info = { req: ctx.request, started_at: Date.now() };
+        info.message = format(msg, info.req.method, info.req.url);
 
-        ctx.log = contextLogger;
-        await next()
-        const responseTime = Date.now() - start;
-        
-        logger.info(access(ctx, {
-            responseTime: `响应时间为${responseTime / 1000}s`
-        }, commonInfo))
-    }
-}
+        let error;
+        try {
+            await next();
+        } catch (e) {
+            // catch and throw it later
+            error = e;
+        } finally {
+            onFinished(ctx.response, onResponseFinished.bind(null, ctx, info));
+        }
+
+        if (error) {
+            throw error;
+        }
+    };
+};
---
+++ server/log/stringify_schema.js
@@ -0,0 +1,208 @@
+const set = require('lodash.set');
+const get = require('lodash.get');
+const mapvalues = require('lodash.mapvalues');
+const clonedeep = require('lodash.clonedeep');
+const fastJson = require('fast-json-stringify');
+
+const PREFIXS = ['req', 'res'];
+
+const defaultSchemas = {
+    res: {
+        title: 'koa2-winston-info-res',
+        type: 'object',
+        properties: {
+            header: {
+                type: 'object',
+                additionalProperties: { type: 'string' },
+            },
+            status: { type: 'string' },
+            body: {
+                type: 'object',
+                additionalProperties: true,
+            },
+        },
+    },
+    req: {
+        title: 'koa2-winston-info-req',
+        type: 'object',
+        properties: {
+            header: {
+                type: 'object',
+                additionalProperties: { type: 'string' },
+            },
+            url: { type: 'string' },
+            method: { type: 'string' },
+            href: { type: 'string' },
+            body: {
+                type: 'object',
+                additionalProperties: { type: 'string' },
+            },
+            query: {
+                type: 'object',
+                additionalProperties: { type: 'string' },
+            },
+            origin: { type: 'string' },
+            originalUrl: { type: 'string' },
+            path: { type: 'string' },
+            querystring: { type: 'string' },
+            search: { type: 'string' },
+            hostname: { type: 'string' },
+            URL: { type: 'string' },
+            type: { type: 'string' },
+            charset: { type: 'string' },
+            protocol: { type: 'string' },
+            secure: { type: 'string' },
+            ip: { type: 'string' },
+            httpVersion: { type: 'string' },
+            length: { type: 'integer' },
+        },
+    },
+    info: {
+        title: 'koa2-winston-info',
+        definitions: {
+            req: {},
+            res: {},
+        },
+        type: 'object',
+        properties: {
+            started_at: { type: 'integer' },
+            duration: { type: 'integer' },
+            level: { type: 'string' },
+            message: { type: 'string' },
+            req: { $ref: '#/definitions/req' },
+            res: { $ref: '#/definitions/res' },
+        },
+    },
+};
+
+const DOT_RE = /\./g;
+/**
+ * @param {string} path
+ */
+const asJsonSchemaPath = path => path.replace(DOT_RE, '.properties.');
+
+/**
+ * @param {object} schema - generated json schema
+ */
+const ensureTypeObject = schema => mapvalues(schema, (value) => {
+    if (typeof value !== 'object') {
+        return value;
+    }
+    if (value.properties) {
+        // eslint-disable-next-line no-param-reassign
+        value.type = 'object';
+    }
+    return ensureTypeObject(value);
+});
+
+/**
+ * @callback schemaKeysHandlerFn
+ * @param {string} path
+ */
+
+/**
+ * @param {string[]} keys - schema keys
+ * @param {schemaKeysHandlerFn} handler - assign path
+ */
+const schemaKeysHandler = (keys, handler) => keys
+    .map(asJsonSchemaPath)
+    .map(path => `properties.${path}`)
+    .map(handler);
+
+const schemaKeysHandlers = ({
+    keys, select, unselect, schema,
+}) => {
+    const outputSchema = { type: 'object', properties: {} };
+    schemaKeysHandler(keys.concat(select), (path) => {
+        set(outputSchema, path, get(schema, path, {}));
+    });
+    schemaKeysHandler(unselect, (path) => {
+        const parentPath = path.match(DOT_RE).length > 2
+            ? path
+                .split('.')
+                .slice(0, -2)
+                .join('.')
+            : path;
+        if (!get(outputSchema, parentPath)) {
+            return;
+        }
+        set(outputSchema, path, { type: 'null' });
+    });
+    return outputSchema;
+};
+
+/**
+ * logger middleware for koa2 use winston
+ *
+ * @param {object} [payload={}] - input arguments
+ * @param {string[]} [payload.reqKeys=['header','url','method','httpVersion', 'href', 'query', 'length']] - default request fields to be logged
+ * @param {string[]} [payload.reqSelect=[]] - additional request fields to be logged
+ * @param {string[]} [payload.reqUnselect=['header.cookie']] - request field will be removed from the log
+ * @param {string[]} [payload.resKeys=['header', 'status']] - default response fields to be logged
+ * @param {string[]} [payload.resSelect=[]] - additional response fields to be logged
+ * @param {string[]} [payload.resUnselect=[]] - response field will be removed from the log
+ */
+const generateSchema = (payload) => {
+    const options = Object.assign(
+        {
+            reqUnselect: ['header.cookie'],
+            reqSelect: [],
+            reqKeys: [
+                'header',
+                'url',
+                'method',
+                'httpVersion',
+                'href',
+                'query',
+                'length',
+            ],
+            resUnselect: [],
+            resSelect: [],
+            resKeys: ['header', 'status'],
+        },
+        payload,
+    );
+
+    const { info: infoSchema } = clonedeep(defaultSchemas);
+    PREFIXS.forEach((prefix) => {
+        infoSchema.definitions[prefix] = schemaKeysHandlers({
+            keys: options[`${prefix}Keys`],
+            select: options[`${prefix}Select`],
+            unselect: options[`${prefix}Unselect`],
+            schema: defaultSchemas[prefix],
+        });
+    });
+
+    ensureTypeObject(infoSchema.definitions);
+    return infoSchema;
+};
+
+const generateFormat = (payload) => {
+    const schema = generateSchema(payload);
+    const stringify = fastJson(schema);
+    const keys = {
+        req: Object.keys(schema.definitions.req.properties),
+        res: Object.keys(schema.definitions.res.properties),
+    };
+    return (info) => {
+        PREFIXS.forEach((prefix) => {
+            const prefixCopy = {};
+            keys[prefix].forEach((key) => {
+                prefixCopy[key] = info[prefix][key];
+            });
+            info[prefix] = prefixCopy;
+        });
+
+        const infoMsg = stringify(info);
+        // rewrite info object as omit function
+        Object.assign(info, JSON.parse(infoMsg));
+        return infoMsg;
+    };
+};
+
+module.exports = {
+    defaultSchemas,
+    generateSchema,
+    generateFormat,
+    asJsonSchemaPath,
+};
